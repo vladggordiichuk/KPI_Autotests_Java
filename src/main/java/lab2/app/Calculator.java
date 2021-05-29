bbb6d07af3b6bdb0beddd9b98e5b3142a4ac0db2
Vlad Gordiichuk <vladyslav.gordiichuk@eltexsoft.com>
2021-05-29 21:48:45 +0300

    first commit

---
+++ src/main/java/lab2/app/Calculator.java
@@ -0,0 +1,7 @@
+package lab2.app;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/lab2/acceptancetests/AcceptanceTestSuite.java
@@ -0,0 +1,9 @@
+package lab2.acceptancetests;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/", glue="lab2")
+public class AcceptanceTestSuite {}
